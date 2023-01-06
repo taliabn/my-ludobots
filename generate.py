74e40f8a0771d40e4d6e2768b54e6a0f2e3151b3
Talia <taliabennaim@gmail.com>
2023-01-05 22:44:29 -0600

    completed twenty five tower

--- generate.py
+++ generate.py
@@ -3,7 +3,9 @@ import pyrosim.pyrosim as pyrosim
 pyrosim.Start_SDF("boxes.sdf")
 length, height, width = 1, 1, 1 # box dimensions
 x, y, z = 0, 0, 0.5 # box starting position
-for i in range(10):
-	pyrosim.Send_Cube(name="Box", pos=[x, y, z + i*10] , 
-					  size=[length*(0.9**i), height*(0.9**i), width*(0.9**i)])
+for i in range(5):	
+	for j in range(5):
+		for k in range(10):
+			pyrosim.Send_Cube(name="Box", pos=[x + i, y + j, z + k] , 
+					  		  size=[length*(0.9**k), height*(0.9**k), width*(0.9**k)])
 pyrosim.End()
