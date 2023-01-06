672aa6f085d590e373073b73627db13d0de788b8
Talia <taliabennaim@gmail.com>
2023-01-05 18:33:49 -0600

    loop to step through simulation

--- simulate.py
+++ simulate.py
@@ -1,4 +1,12 @@
-import pybullet as p
+import pybullet as p # type: ignore
+import time
 
 physicsClient = p.connect(p.GUI)
+
+for i in range(1000):
+	time.sleep(1/60)
+	p.stepSimulation()
+	print(i)
+
+
 p.disconnect()
