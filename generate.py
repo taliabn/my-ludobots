fad4bbad32c9eb7530a5356b0e74990be1d13341
Talia <taliabennaim@gmail.com>
2023-01-05 22:29:10 -0600

    added second link

--- generate.py
+++ generate.py
@@ -4,4 +4,5 @@ pyrosim.Start_SDF("box.sdf")
 length, height, width = 1, 1, 1 # box dimensions
 x, y, z = 0, 0, 0.5 # box starting position
 pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, height, width])
+pyrosim.Send_Cube(name="Box2", pos=[x, y, z] , size=[length, height, width])
 pyrosim.End()
