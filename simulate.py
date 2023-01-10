e225ed69a0835f7e7d02482f61901bd4ea4a56d4
Talia <taliabennaim@gmail.com>
2023-01-10 17:42:34 -0600

    added front leg sensor

--- simulate.py
+++ simulate.py
@@ -4,6 +4,8 @@ import pybullet_data # type: ignore
 import pybullet as p # type: ignore
 import pyrosim.pyrosim as pyrosim
 
+# number of times to step through simulation
+steps = 1000
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -12,15 +14,17 @@ planeId = p.loadURDF("plane.urdf") # floor
 robotId = p.loadURDF("body.urdf") # floor
 p.loadSDF("world.sdf")
 
-backLegSensorValues = np.zeros(10000)
-print(backLegSensorValues)
+backLegSensorValues = np.zeros(steps)
+frontLegSensorValues = np.zeros(steps)
 
 pyrosim.Prepare_To_Simulate(robotId)
 # run simulation
-for i in range(1000):
+for i in range(steps):
 	time.sleep(1/60)
 	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+	frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 	p.stepSimulation()
 	# print(i)
 p.disconnect()
 np.save("./data/backLegSensorValues", backLegSensorValues)
+np.save("./data/frontLegSensorValues", frontLegSensorValues)
