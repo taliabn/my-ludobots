db272713ed059729e6d0fada83a628eb1ddc2842
Talia <taliabennaim@gmail.com>
2023-01-22 23:01:30 -0600

    fully connected nn with random initial synapse values

--- generate.py
+++ generate.py
@@ -1,5 +1,5 @@
 import pyrosim.pyrosim as pyrosim
-
+import random
 
 def Create_World():
 	pyrosim.Start_SDF("world.sdf")
@@ -35,10 +35,10 @@ def Generate_Brain():
 	# create motor neurons
 	pyrosim.Send_Motor_Neuron( name = 3, jointName = "Torso_BackLeg")
 	pyrosim.Send_Motor_Neuron( name = 4, jointName = "Torso_FrontLeg")
-
 	# create synapses
-	pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = 10.0 )
-	pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1.0 )
+	for sensor_neuron in range(3):
+		for motor_neuron in range(3,5):
+			pyrosim.Send_Synapse( sourceNeuronName = sensor_neuron , targetNeuronName = motor_neuron , weight =  random.random()*2 + -1 )
 
 	pyrosim.End()
 
