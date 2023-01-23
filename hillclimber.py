04492da39d4c7ac527ab64884f02d51833ebe3a3
Talia <taliabennaim@gmail.com>
2023-01-23 12:39:47 -0600

    added template for evolution

--- hillclimber.py
+++ hillclimber.py
@@ -1,5 +1,6 @@
 from solution import SOLUTION
 import numpy as np
+import constants as c
 
 class HILL_CLIMBER:
 
@@ -9,3 +10,26 @@ class HILL_CLIMBER:
 
 	def Evolve(self):
 		self.parent.Evaluate()
+		for currentGeneration in range(c.numberOfGenerations):
+			self.Evolve_For_One_Generation()
+
+	
+	def Evolve_For_One_Generation(self):
+		self.Spawn()
+		self.Mutate()
+		self.child.Evaluate()
+		self.Select()
+
+	
+	def Spawn(self):
+		pass
+
+	
+	def Mutate(self):
+		pass
+
+	
+	def Select(self):
+		pass
+
+	
