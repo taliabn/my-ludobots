5be37be62612913f3ccf5a5c6f21b3ed3e2b5eb8
Talia <taliabennaim@gmail.com>
2023-01-28 00:03:15 -0600

    initialized parallel hillclimber class

---
+++ parallelhillclimber.py
@@ -0,0 +1,53 @@
+from solution import SOLUTION
+import numpy as np
+import constants as c
+import copy
+
+class PARALLEL_HILL_CLIMBER:
+
+	def __init__(self):
+		self.parents = {}
+		for i in range(c.populationSize):
+			self.parents.update({i:SOLUTION()})
+
+
+	def Evolve(self):
+		for parent in self.parents.values():
+			print(parent)
+			parent.Evaluate("GUI")
+		# self.parent.Evaluate("GUI")
+		# for currentGeneration in range(c.numberOfGenerations):
+		# 	self.Evolve_For_One_Generation()
+
+	
+	def Evolve_For_One_Generation(self):
+		self.Spawn()
+		self.Mutate()
+		self.child.Evaluate("DIRECT")
+		self.Print()
+		self.Select()
+
+	
+	def Spawn(self):
+		self.child = copy.deepcopy(self.parent)
+		pass
+
+	
+	def Mutate(self):
+		self.child.Mutate()
+
+	
+	def Select(self):
+		# parent's fitness is worse than child's
+		if self.parent.fitness > self.child.fitness:
+			self.parent = self.child
+		pass
+
+
+	def Print(self):
+		print(f"\nparent's fitness: {self.parent.fitness}, child's fitness: {self.child.fitness}")
+
+	
+	def Show_Best(self):
+		pass
+		# self.parent.Evaluate("GUI")
--- search.py
+++ search.py
@@ -1,9 +1,9 @@
 import os
-from hillclimber import HILL_CLIMBER
+from parallelhillclimber import PARALLEL_HILL_CLIMBER
 
-hc = HILL_CLIMBER()
-hc.Evolve()
-hc.Show_Best()
+phc = PARALLEL_HILL_CLIMBER()
+phc.Evolve()
+phc.Show_Best()
 # for i in range(5):
 # 	os.system("python generate.py")
 # 	os.system("python simulate.py")
