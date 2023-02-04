567fce9843121b33e19d3074936841de87f6c553
Talia <taliabennaim@gmail.com>
2023-02-04 13:52:05 -0600

    changed max force

--- motor.py
+++ motor.py
@@ -16,4 +16,4 @@ class MOTOR:
 									jointName = self.jointName,
 									controlMode =  p.POSITION_CONTROL,
 									targetPosition = desiredAngle,
-									maxForce = 25)		
+									maxForce = 50)		
