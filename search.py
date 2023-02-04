3061d992326d898e98299bd2c74a5d927744d128
Talia <taliabennaim@gmail.com>
2023-02-03 18:49:22 -0600

    added error catching for "PermissionDenied"

--- search.py
+++ search.py
@@ -2,5 +2,9 @@ from parallelhillclimber import PARALLEL_HILL_CLIMBER
 
 
 phc = PARALLEL_HILL_CLIMBER()
-phc.Evolve()
-phc.Show_Best()
+try:
+	phc.Evolve()
+except PermissionError:
+	print("ERROR: permission error")
+finally:
+	phc.Show_Best()
