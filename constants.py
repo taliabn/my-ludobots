31b2f69edde73c98b64c6224cd457f31be04aed4
Talia <taliabennaim@gmail.com>
2023-02-05 15:33:34 -0600

    final constants for final project

--- constants.py
+++ constants.py
@@ -1,17 +1,18 @@
 # number of evolutions
-populationSize = 1
-numberOfGenerations = 1
+populationSize = 10
+numberOfGenerations = 100
 # number of times to step through simulation
-steps = 2000
+steps = 3000
 # length of each time-step in seconds
 stepLength = 1/360
 # nn properties
-numSensorNeurons = 6
-numMotorNeurons = numSensorNeurons - 1
+numSensorNeurons = 4
+numHiddenNeurons = 4
+numMotorNeurons = 4
 motorJointRange = 1
 # pyramid dimensions
-gap_width = 1.5
-pyramid_x = -7.6
+gap_width = 1.75
+pyramid_x = -7.75
 num_pyramid_layers = 10
 pyramid_length = 15
-layer_height = 0.25
+layer_height = 0.1
