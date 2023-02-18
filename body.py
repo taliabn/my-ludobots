689760282f7851c8ade171a9ddc984c556509715
Talia <taliabennaim@gmail.com>
2023-02-18 17:43:26 -0600

    generate 3D bodies with recursion and genotype graph

---
+++ body.py
@@ -0,0 +1,149 @@
+import pyrosim.pyrosim as pyrosim
+import numpy as np
+import os
+import random
+import constants as c
+
+
+class uniqueNode:
+
+	def __init__(self, numSelfEdge, numChildEdge, orientation):
+		# from dna:
+		self.numSelfEdge: int = numSelfEdge
+		self.numChildEdge: int = numChildEdge
+		# randomly chosen:
+		self.has_sensor: bool = bool(random.getrandbits(1)) # random
+		self.orientation = orientation # random
+		# possibly change min/max side length to be encoded in dna?
+		# dimensions
+		# self.l, self.w, self.h: float = [random.uniform(c.minSideLen, c.maxSideLen)
+		# 									for dim in range(3)] #random
+		self.dims = np.array([random.uniform(c.minSideLen, c.maxSideLen)
+											for dim in range(3)]) #random
+		if self.has_sensor:
+			self.color = "Green"
+		else:
+			self.color = "Blue"
+
+
+	def axisStr(self):
+		return f"{self.orientation[0]} {self.orientation[1]} {self.orientation[2]}"
+
+
+class BODY:
+
+	def __init__(self, dna, ID):
+		print("INITIALIZING BODY CLASS INSTANCE")
+		self.myID = ID
+		self.dna = dna # list of [num selfEdges, num nextEdges] pairs
+		# initialize graph nodes
+		self.uniqueNodeList = [uniqueNode(instruction[0], instruction[1], self.randomOrientation())
+							  	  for instruction in self.dna]
+		self.sensorLinks = [] # listof node names (str)
+		self.allLinks = []
+		self.numLinks = 1 # root
+		self.Generate_Body()
+		self.numSensorNeurons = len(self.sensorLinks)
+
+
+	def switchDirection(self, dir, orientation):
+		orientation *= -1
+		return np.logical_not(orientation)*dir + orientation
+
+
+	def randomOrientation(self):
+		return np.array(random.choice([[1,0,0], [0,1,0], [0,0,1]]))
+
+
+	def Generate_Body(self):
+		# function to recursively add links
+		def add_link(prevUniqueNode,  prevClone, prevChild, currUniqueNode, currClone, currChild, growthDir):
+			# print(locals(), "\n")
+			currNode = self.uniqueNodeList[currUniqueNode]
+			prevNode = self.uniqueNodeList[prevUniqueNode]
+
+			currJointPos = prevNode.dims * (prevNode.orientation + currNode.orientation) * growthDir 
+			currLinkPos = currNode.dims * (currNode.orientation) * growthDir
+
+			prevLinkName = f"{prevUniqueNode}A{prevClone}A{prevChild}"
+			currLinkName = f"{currUniqueNode}A{currClone}A{currChild}"
+			print(f"{prevLinkName}_{currLinkName}" )
+			pyrosim.Send_Joint(name=f"{prevLinkName}_{currLinkName}" , 
+							   parent=f"{prevLinkName}",
+							   child=f"{currLinkName}",
+							   type="revolute", 
+							   position=currJointPos, 
+							   jointAxis=currNode.axisStr())
+			pyrosim.Send_Cube(name=currLinkName, 
+							  pos=currLinkPos, 
+							  size=currNode.dims, 
+							  color=currNode.color)
+			# maintain invariants
+			self.numLinks += 1
+			if currNode.has_sensor:
+				self.sensorLinks.append(currLinkName)	
+			self.allLinks.append(currLinkName)
+
+			# recursive call(s) to add clones
+			if currClone < currNode.numSelfEdge :
+
+				add_link(prevUniqueNode = currUniqueNode, prevClone = currClone, prevChild = currClone,
+						 currUniqueNode = currUniqueNode, currClone = currClone + 1, currChild = currChild,
+						 growthDir = growthDir)
+			# recursive call(s) to add children
+			if len(self.uniqueNodeList) > currUniqueNode + 1:
+				for child in range(currNode.numChildEdge):
+					# branch in new growth direction along orientation if this is the second child
+					if child == 1:
+						newDir = self.switchDirection(dir=growthDir, orientation=currNode.orientation)
+					else:
+						newDir = growthDir					
+					add_link(prevUniqueNode = currUniqueNode, prevClone = currClone, prevChild = currClone,
+							currUniqueNode = currUniqueNode + 1, currClone = currClone, currChild = child,
+							growthDir = newDir)
+
+		pyrosim.Start_URDF(f"body{self.myID}.urdf") # stores description of robot's body
+		print(f"body{self.myID}.urdf")
+		# add root link
+		currNode = self.uniqueNodeList[0]
+		# LINK NAMING CONVENTION: uniqueNodeIndex-self-child	
+		pyrosim.Send_Cube(name="0A0A0", 
+						  pos=currNode.dims/2, 
+						  size=currNode.dims, 
+						  color="Green")
+		# root link always has a sensor, even if uniqueNode 0 does not
+		self.sensorLinks.append("0A0A0")
+		growthDir = [1,1,1]
+		# add clones
+		# print(currNode.numChildEdge, currNode.numSelfEdge, currNode)
+		if currNode.numSelfEdge > 0:
+			add_link(prevUniqueNode = 0, prevClone = 0, prevChild = 0,
+						currUniqueNode = 0, currClone = 1, currChild = 0,
+						growthDir = growthDir)
+		# add children
+		if len(self.uniqueNodeList) > 2:
+			for child in range(currNode.numChildEdge): # assumed to be 1 or 2
+
+				# branch in new growth direction along orientation if this is the second child
+				if child == 1:
+					newDir = self.switchDirection(dir=growthDir, orientation=currNode.orientation)
+				else:
+					newDir = growthDir
+				add_link(prevUniqueNode = 0, prevClone = 0, prevChild = 0,
+							currUniqueNode = 1, currClone = 0, currChild = child,
+							growthDir = newDir)
+		print("\n\nESCAPED RECURSION\n")
+		pyrosim.End()
+		print("ENDED PYROSIM\n")
+
+
+# TODO:
+	# if leaf, add sensor
+	# naming conventions
+	# jointaxis / type
+	# consolidate randoms (random lib vs numpy)
+
+# tradeoffs:
+	#linear each unique node only connects to one unique node (linear)
+	# root always has sensor
+	# 2 children edges max
--- solution.py
+++ solution.py
@@ -4,20 +4,24 @@ import os
 import random
 import time
 import constants as c
+from body import BODY
 
 
 class SOLUTION:
 
 	def __init__(self, ID):
-
+		self.dna = [[3,2],[1,1]]	
 		self.myID = ID
-		self.sensorLinks = []
-		self.numLinks = random.randint(2, c.maxNumLinks)
+		self.body = BODY(self.dna, self.myID)
+		self.Generate_Random_Brain()
+		# self.sensorLinks = []
+		# self.numLinks = random.randint(2, c.maxNumLinks)
+		
 
 
 	def Start_Simulation(self, directOrGUI):
-		self.Generate_Random_Body()
-		self.Generate_Random_Brain()
+		# self.body = BODY(self.dna, self.myID)
+		# self.Generate_Random_Brain()
 		IDstr = str(self.myID)
 		os.system("start /B python simulate.py " + directOrGUI + " " + IDstr) # OS specific call
 
@@ -35,60 +39,62 @@ class SOLUTION:
 		return self.fitness
 
 
-	def Generate_Random_Body(self):
-		pyrosim.Start_URDF(f"body.urdf") # stores description of robot's body
+	# def Generate_Random_Body(self):
+	# 	if os.path.exists(f"body{self.myID}.urdf"):
+	# 		return
+	# 	pyrosim.Start_URDF(f"body{self.myID}.urdf") # stores description of robot's body
 		
-		sensorColor = {0: "Blue", 1: "Green"}
-		l, w, h = [random.uniform(c.minSideLen, c.maxSideLen) for dim in range(3)]
-		has_sensor = random.randint(0,1)
-		pyrosim.Send_Cube(name="0", pos=[l/2, w/2, h/2], size=[l, w, h], color=sensorColor[has_sensor])
-		if has_sensor:
-			self.sensorLinks.append("0")	
+	# 	sensorColor = {0: "Blue", 1: "Green"}
+	# 	l, w, h = [random.uniform(c.minSideLen, c.maxSideLen) for dim in range(3)]
+	# 	has_sensor = random.randint(0,1)
+	# 	pyrosim.Send_Cube(name="0", pos=[l/2, w/2, h/2], size=[l, w, h], color=sensorColor[has_sensor])
+	# 	if has_sensor:
+	# 		self.sensorLinks.append("0")	
 
-		for i in range(1, self.numLinks):
-			pyrosim.Send_Joint(name = f"{i-1}_{i}" , parent= f"{i-1}" , child = f"{i}" , 
-							   type = "revolute", position = [l, w, 0], jointAxis = "0 0 1") # prev block pos
-			l, w, h = [random.uniform(c.minSideLen, c.maxSideLen) for dim in range(3)]
-			has_sensor = random.randint(0,1)		
-			pyrosim.Send_Cube(name=f"{i}", pos=[l/2, w/2, h/2], size=[l, w, h], color=sensorColor[has_sensor])
-			if has_sensor:
-				self.sensorLinks.append(f"{i}")		
+	# 	for i in range(1, self.numLinks):
+	# 		pyrosim.Send_Joint(name = f"{i-1}_{i}" , parent= f"{i-1}" , child = f"{i}" , 
+	# 						   type = "spherical", position = [l, w, 0], jointAxis = "0 0 1") # prev block pos
+	# 		l, w, h = [random.uniform(c.minSideLen, c.maxSideLen) for dim in range(3)]
+	# 		has_sensor = random.randint(0,1)		
+	# 		pyrosim.Send_Cube(name=f"{i}", pos=[l/2, w/2, h/2], size=[l, w, h], color=sensorColor[has_sensor])
+	# 		if has_sensor:
+	# 			self.sensorLinks.append(f"{i}")		
 
-		self.numSensorNeurons = len(self.sensorLinks)
-		pyrosim.End()
+	# 	self.numSensorNeurons = len(self.sensorLinks)
+	# 	pyrosim.End()
 
 
 	def Generate_Random_Brain(self):
 		pyrosim.Start_NeuralNetwork(f"brain{self.myID}.nndf") # stores description of neural network
 
 		# create sensor neurons
-		for i, link in enumerate(self.sensorLinks):
+		for i, link in enumerate(self.body.sensorLinks):
 			# print(f"sensor neuron {i} for link {link}")
 			pyrosim.Send_Sensor_Neuron(name = i, linkName = f"{link}")
 		# create hidden neurons
-		for i in  range(self.numSensorNeurons, self.numSensorNeurons + c.numHiddenNeurons):
+		for i in  range(self.body.numSensorNeurons, self.body.numSensorNeurons + c.numHiddenNeurons):
 			pyrosim.Send_Hidden_Neuron(name = i)
 		# create motor neurons
-		for i in range(1, self.numLinks):
-			pyrosim.Send_Motor_Neuron(name = i + self.numSensorNeurons + c.numHiddenNeurons,
+		for i in range(1, self.body.numLinks):
+			pyrosim.Send_Motor_Neuron(name = i + self.body.numSensorNeurons + c.numHiddenNeurons,
 			     					  jointName = f"{i-1}_{i}")
 		
 		# initialize neuron weights
-		self.weights = [np.random.rand(c.numHiddenNeurons, self.numSensorNeurons) * 2 - 1,
-						np.random.rand(c.numHiddenNeurons, self.numLinks) * 2 - 1,]
+		self.weights = [np.random.rand(c.numHiddenNeurons, self.body.numSensorNeurons) * 2 - 1,
+						np.random.rand(c.numHiddenNeurons, self.body.numLinks) * 2 - 1,]
 		
 		# create synapses for sensor neurons to hidden neurons
 		for currRow in range(c.numHiddenNeurons):
-			for currColumn in range(self.numSensorNeurons):
+			for currColumn in range(self.body.numSensorNeurons):
 				pyrosim.Send_Synapse(sourceNeuronName = currColumn, 
-			 						targetNeuronName = currRow + self.numSensorNeurons, 
+			 						targetNeuronName = currRow + self.body.numSensorNeurons, 
 									weight = self.weights[0][currRow][currColumn])
 
 		# create synapses for hidden neurons to motor neurons
 		for currRow in range(c.numHiddenNeurons):
-			for currColumn in range(self.numLinks):
-				pyrosim.Send_Synapse(sourceNeuronName = currRow + self.numSensorNeurons, 
-			 						targetNeuronName = currColumn + self.numSensorNeurons + c.numHiddenNeurons, 
+			for currColumn in range(self.body.numLinks):
+				pyrosim.Send_Synapse(sourceNeuronName = currRow + self.body.numSensorNeurons, 
+			 						targetNeuronName = currColumn + self.body.numSensorNeurons + c.numHiddenNeurons, 
 									weight = self.weights[1][currRow][currColumn])
 				
 		pyrosim.End()
@@ -96,11 +102,12 @@ class SOLUTION:
 
 	def Mutate(self):
 		randomRow = random.randint(0, c.numHiddenNeurons - 1)
-		# change weight of a random sensor neuron synapse
-		randomSensorColumn = random.randint(0, self.numSensorNeurons - 1)
-		self.weights[0][randomRow][randomSensorColumn] = random.random() * 2 - 1
+		# change weight of a random sensor neuron synapse, if at least one exists
+		if self.numSensorNeurons > 0:
+			randomSensorColumn = random.randint(0, self.body.numSensorNeurons - 1)
+			self.weights[0][randomRow][randomSensorColumn] = random.random() * 2 - 1
 		# change weight of a random motor neuron synapse
-		randomMotorColumn = random.randint(0, self.numSensorNeurons - 1)
+		randomMotorColumn = random.randint(0, self.body.numLinks - 1)
 		self.weights[1][randomRow][randomMotorColumn] = random.random() * 2 - 1
 
 
