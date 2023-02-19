37d846e7ca666b47f2363f1cd360bef9ca453846
Talia <taliabennaim@gmail.com>
2023-02-19 12:16:01 -0600

    joint and link positioning working for case of single uniqueNode

--- body.py
+++ body.py
@@ -18,13 +18,16 @@ class uniqueNode:
 		# dimensions
 		# self.l, self.w, self.h: float = [random.uniform(c.minSideLen, c.maxSideLen)
 		# 									for dim in range(3)] #random
-		self.dims = np.array([random.uniform(c.minSideLen, c.maxSideLen)
+		self.dims = np.array([round(random.uniform(c.minSideLen, c.maxSideLen), 2)
 											for dim in range(3)]) #random
 		if self.has_sensor:
 			self.color = "Green"
 		else:
 			self.color = "Blue"
 
+	def Print(self):
+		print(f"class uniqueNode:\n\tnumSelfEdge: {self.numSelfEdge}\n\tnumChildEdge: {self.numChildEdge} \
+			\n\thas_sensor: {self.has_sensor}\n\torientation: {self.orientation}\n\tdims: {self.dims}\n")
 
 	def axisStr(self):
 		return f"{self.orientation[0]} {self.orientation[1]} {self.orientation[2]}"
@@ -39,6 +42,8 @@ class BODY:
 		# initialize graph nodes
 		self.uniqueNodeList = [uniqueNode(instruction[0], instruction[1], self.randomOrientation())
 							  	  for instruction in self.dna]
+		for node in self.uniqueNodeList:
+			node.Print()
 		self.sensorLinks = [] # listof node names (str)
 		self.allLinks = []
 		self.availableID = 0
@@ -51,12 +56,12 @@ class BODY:
 		return self.availableID
 
 	def switchDirection(self, dir, orientation):
-		orientation *= -1
-		return np.logical_not(orientation)*dir + orientation
+		# orientation *= -1
+		return np.logical_not(orientation)*dir + orientation*-1
 
 
 	def randomOrientation(self):
-		return np.array(random.choice([[1,0,0], [0,1,0], [0,0,1]]))
+		return np.array(random.choice([[0,1,0], [1,0,0], [0,0,1]]))
 
 
 	def Generate_Body(self):
@@ -66,12 +71,16 @@ class BODY:
 			currNode = self.uniqueNodeList[currUniqueNode]
 			prevNode = self.uniqueNodeList[prevUniqueNode]
 
-			currJointPos = prevNode.dims * (prevNode.orientation + currNode.orientation) * growthDir 
-			currLinkPos = currNode.dims * (currNode.orientation) * growthDir
+			currJointPos = (prevNode.orientation + currNode.orientation)  * prevNode.dims/2 * growthDir 
+			currLinkPos = (currNode.orientation) * currNode.dims/2 * growthDir
+			# currJointPos =  (prevNode.orientation + currNode.orientation) * prevNode.dims/2
+			# currLinkPos =  (currNode.orientation) * currNode.dims/2
 
 			currLinkName = f"{currUniqueNode}-{self.fetchID()}"
 			# print(f"CURR: {currLinkName} ")
 			print(f"{prevLinkName}_{currLinkName}" )
+			print(f"currJointPos: {currJointPos}" )
+			print(f"currLinkPos: {currLinkPos}" )
 			pyrosim.Send_Joint(name=f"{prevLinkName}_{currLinkName}" , 
 							   parent=f"{prevLinkName}",
 							   child=f"{currLinkName}",
@@ -95,7 +104,7 @@ class BODY:
 						 currUniqueNode = currUniqueNode, currClone = currClone + 1, currChild = currChild,
 						 growthDir = growthDir)
 			# recursive call(s) to add children
-			if len(self.uniqueNodeList) > currUniqueNode + 1:
+			if currUniqueNode + 1 < len(self.uniqueNodeList):
 				for child in range(currNode.numChildEdge):
 					# branch in new growth direction along orientation if this is the second child
 					if child == 1:
@@ -128,7 +137,7 @@ class BODY:
 						currUniqueNode = 0, currClone = 1, currChild = 0,
 						growthDir = growthDir)
 		# add children
-		if len(self.uniqueNodeList) > 2:
+		if 2 < len(self.uniqueNodeList):
 			for child in range(currNode.numChildEdge): # assumed to be 1 or 2
 
 				# branch in new growth direction along orientation if this is the second child
