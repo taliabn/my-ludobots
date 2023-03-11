106cd4c29efbb2cafc8bd8bf30895046b0c9076b
Talia <taliabennaim@gmail.com>
2023-03-11 12:07:29 -0600

    constants for final project

--- constants.py
+++ constants.py
@@ -1,24 +1,24 @@
 # number of evolutions
-populationSize = 30
-numberOfGenerations = 150
+populationSize = 21
+numberOfGenerations = 250
 # number of times to step through simulation
-steps = 1000
+steps = 750
 # length of each time-step in seconds
 stepLength = 1/120
 # nn properties
 numHiddenNeurons = 4
 # motor properties
 motorJointRange = 1
-maxMotorForce = 30
+maxMotorForce = 20
 # pyramid dimensions
 gap_width = 1.75
-pyramid_x = -7.75
+pyramid_x = -8
 num_pyramid_layers = 10
 pyramid_length = 15
-layer_height = 0.1
+layer_height = 0.2
 # random body plan ranges
 maxNumNodes = 4
-maxNumSelfEdges = 3
+maxNumSelfEdges = 2
 maxNumChildEdges = 2
 minSideLen = 0.1
 maxSideLen = .75
